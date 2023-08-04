5a3619a3003ead4f6ea1b66f7c1fb9f256aa4213
joy.chang <joychang1266@gmail.com>
2023-08-04 21:08:36 +0800

    vault backup: 2023-08-04 21:08:36

--- managementSystem/src/main/java/com/example/managementSystem/controller/UserController.java
+++ managementSystem/src/main/java/com/example/managementSystem/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.example.managementSystem.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +22,43 @@ public class UserController {
 	
 	@PostMapping("/insertUser")
 	public String insert(@RequestBody User user) {
+		
 		userRespository.save(user);
-		return "執行資料庫的 Create 動作";
+		
+		return "Create User";
 		
 	}
+	
+	@PutMapping("/users/{userId}")
+	public String update(@PathVariable Integer userId,
+						 @RequestBody User user) {
+		
+		User u = userRespository.findById(userId).orElse(null);
+		
+		if (u != null) {
+			u.setName(user.getName());
+			userRespository.save(u);
+			
+			return "Update User";
+		} else {
+			return "Update fail, User not found!";
+		}
+		
+	}
+	
+	@DeleteMapping("/users/{userId}")
+	public String delete(@PathVariable Integer userId) {
+		
+		userRespository.deleteById(userId);
+		return "Delete User";
+	}
+	
+	@GetMapping("/users/{userId}")
+	public  User findUser(@PathVariable Integer userId) {
+		
+		User user = userRespository.findById(userId).orElse(null);
+		return user;
+	}
+	
 
 }
